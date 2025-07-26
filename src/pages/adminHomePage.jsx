5012b01fe88b0773033af0e2f85add72ed38b23f
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-26 19:29:56 +0530

    Enhance AdminHomePage layout with navigation links and Tailwind CSS styling. Added sidebar for dashboard, products, orders, and customers, improving user navigation experience.

--- src/pages/adminHomePage.jsx
+++ src/pages/adminHomePage.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function AdminHomePage(){
     return(
-        <div>
-            <h1> Admin Home Page </h1>
+        <div className="bg-green-600 w-full h-screen flex">
+
+            <div className="w-[20%] h-screen bg-blue-500">
+                
+                <Link to="/admin/dashboard"> Dashboard  </Link>
+                <Link to="/admin/products"> Products  </Link>
+                <Link to="/admin/order"> Order  </Link>
+                <Link to="/admin/customers"> Customers  </Link>
+                
+            </div>
+
+            <div className="w-[80%] h-screen bg-red-400">
+
+            </div>
+            
         </div>
     )
 }
