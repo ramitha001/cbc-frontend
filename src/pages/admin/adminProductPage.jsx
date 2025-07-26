3fe0385b24640d2aaa2d251b7d2b0ac4ba3b060d
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-26 21:59:37 +0530

    Add axios dependency to package.json and package-lock.json; implement API call in AdminProductPage for product data retrieval.

--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -1,7 +1,11 @@
+import axios from "axios"
+
 export default function AdminProductPage (){
 
 
-    
+    axios.get("http://localhost:3000/product").then((res) =>{
+        console.log(res)
+    })
 
     return(
         <div>
