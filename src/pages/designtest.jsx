ca505073cddedf90f955a2dd697e65c696125ba0
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-27 12:22:43 +0530

    Update DesignTest component to initialize email and password input fields with default values for better user interaction.

--- src/pages/designtest.jsx
+++ src/pages/designtest.jsx
@@ -1,16 +1,22 @@
+import { useState } from "react"
 
 
 export default function DesignTest(){
+
+    const [email,setEmail] = useState("Your Email")
+    const [password,setPassword] = useState("")
     return (
         <div className="flex justify-center items-center w-full h-screen bg-green-500">
 
             <div className="w-[450px] h-[450px] bg-yellow-700 border-8 border-amber-200 flex flex-col justify-center items-center">
 
                 <img src="/mainlogo.jpg" className="rounded-full w-[100px]" />
+
                 <span>Email</span>
-                <input className="bg-white" />
+                <input defaultValue={email} className="bg-white" />
+
                 <span>Password</span>
-                <input type="password" className="bg-white" />
+                <input type="password" defaultValue={password} className="bg-white" />
 
                 <button className="bg-amber-300">Login</button>
             </div>
