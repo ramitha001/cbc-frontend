79201608c523b521c6edbbfa89aef46a799b43fd
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-27 12:05:38 +0530

    Refactor DesignTest component layout by adding input fields for email and password, and adjust styling for better alignment and user experience.

--- src/pages/designtest.jsx
+++ src/pages/designtest.jsx
@@ -4,10 +4,13 @@ export default function DesignTest(){
     return (
         <div className="flex justify-center items-center w-full h-screen bg-green-500">
 
-            <div className="w-[450px] h-[450px] bg-yellow-700 border-8 border-amber-200 ">
+            <div className="w-[450px] h-[450px] bg-yellow-700 border-8 border-amber-200 flex flex-col justify-center items-center">
 
-                <img src="/mainlogo.jpg" className="rounded-full" />
-
+                <img src="/mainlogo.jpg" className="rounded-full w-[100px]" />
+                <span>Email</span>
+                <input />
+                <span>Password</span>
+                <input type="password" />
             </div>
                 
         </div>
