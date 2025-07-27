ed09de1252cc2379bb7da3578638babee55a6656
Ramitha Guruge <ramithaguruge@gmail.com>
2025-07-27 18:16:05 +0530

    Implement product display in AdminProductPage component with hardcoded product data and initial API call setup.

--- src/pages/admin/adminProductPage.jsx
+++ src/pages/admin/adminProductPage.jsx
@@ -1,15 +1,57 @@
 import axios from "axios"
+import { useState } from "react"
 
 export default function AdminProductPage (){
 
 
-    axios.get("http://localhost:3000/product").then((res) =>{
-        console.log(res)
-    })
+    const [product,setProduct] = useState([
+        {
+            "_id": "686f4dec974a7503e3e60ec2",
+            "productID": "B1002",
+            "productName": "Hydrating Glow Serum",
+            "altNames": [
+                "Face Serum",
+                "Glow Booster",
+                "Hydration Elixir"
+            ],
+            "images": [
+                "https://example.com/images/glow-serum-front.jpg",
+                "https://example.com/images/glow-serum-back.jpg"
+            ],
+            "price": 3490,
+            "lastPrice": "3990",
+            "description": "A lightweight, fast-absorbing serum enriched with hyaluronic acid and vitamin C to give your skin a radiant glow and deep hydration.",
+            "__v": 0
+        }
+    ])
+
+    console.log(product)
+
+    
 
     return(
         <div>
-                <h1>Admin hello</h1>
+                <h1>Admin Product page</h1>
+                {
+                    product.map(
+                        (product,index)=>{
+                            return(
+                                <div key={product._id}>
+                                    {index}
+                                    {product.productName}
+                                </div>
+
+                            )
+                        }
+                    )
+                }
         </div>
     )
 }
+
+async function getProducts(con) {
+    const res = await axios.get("http://localhost:3000/product")
+        console.log(res)
+}
+
+
